9b6b2bfc770a4aa16fcaa7808cd50e5daa6f5010
school17729 <school17729@gmail.com>
2024-07-25 23:10:55 -0400

    Added half function

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,3 +1,15 @@
 // Christopher Kong
 // CIS 1202 801
 // July 25th 2024
+
+template <typename T> T half(T value) {
+    return value / 2;
+}
+
+template <> int half(int value) {
+    return static_cast<int>((value / 2.0) + 0.5);
+}
+
+int main() {
+    return 0;
+}
