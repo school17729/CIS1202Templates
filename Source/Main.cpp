dfa99d4818ecee4abc4bbc77fd6e8136041a2df0
school17729 <school17729@gmail.com>
2024-07-25 23:15:37 -0400

    Added a test function template for use as a driver

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,14 +2,26 @@
 // CIS 1202 801
 // July 25th 2024
 
-template <typename T> T half(T value) {
+#include <iostream>
+
+using namespace std;
+
+template<typename T> T half(T value) {
     return value / 2;
 }
 
-template <> int half(int value) {
+template<> int half(int value) {
     return static_cast<int>((value / 2.0) + 0.5);
 }
 
+template<typename T> void test(T value) {
+    cout << "The value " << value << " is passed into the half function." << endl;
+    cout << "The result is " << half(value) << endl;
+}
+
 int main() {
+    test(7.0);
+    test(5.0f);
+    test(3);
     return 0;
 }
